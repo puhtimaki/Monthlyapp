52c0406480fc4d680b5aa2a8880472f6097b93e6
puhtimaki <113307512+puhtimaki@users.noreply.github.com>
2023-02-01 21:36:16 +0200

    added a function for budget

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+let lainaStatis = (document.querySelector('#laina').innerHTML = 263)
+let autoStatic = (document.querySelector('#auto').innerHTML = 500)
+let kauppaStatic = (document.querySelector('#kauppa').innerHTML = 350)
+let muutStatic = (document.querySelector('#muut').innerHTML = 40)
+let viihdeStatic = (document.querySelector('#viihde').innerHTML = 15)
+let nettiStatic = (document.querySelector('#netti').innerHTML = 27)
+
+//onclick event button
+let AddbbudgetButton = document.querySelector('#total-amount-button')
+AddbbudgetButton.addEventListener('click', addBudget)
+
+function addBudget() {
+  let budget = document.querySelector('#total-amount').value
+  let budgetTotal = document.querySelector('#amount')
+  budgetTotal.innerHTML = budget
+  document.querySelector('#total-amount').value = ''
+}
