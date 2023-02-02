df15bba1f4246681034299c7f8a201e7c2a78e98
puhtimaki <113307512+puhtimaki@users.noreply.github.com>
2023-02-02 21:10:32 +0200

    enter key adding

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//statuc expenses
 let lainaStatis = (document.querySelector('#laina').innerHTML = 263)
 let autoStatic = (document.querySelector('#auto').innerHTML = 500)
 let kauppaStatic = (document.querySelector('#kauppa').innerHTML = 350)
@@ -10,7 +11,15 @@ let nettiStatic = (document.querySelector('#netti').innerHTML = 27)
 //onclick event button
 let AddbbudgetButton = document.querySelector('#total-amount-button')
 AddbbudgetButton.addEventListener('click', addBudget)
-
+// eenter input
+let budgetInput = document.querySelector('#total-amount')
+budgetInput.addEventListener('keyup', (e) => {
+  e.preventDefault()
+  if (e.keyCode === 13) {
+    AddbbudgetButton.click()
+  }
+})
+//add budget amount
 function addBudget() {
   let budget = document.querySelector('#total-amount').value
   let budgetTotal = document.querySelector('#amount')
