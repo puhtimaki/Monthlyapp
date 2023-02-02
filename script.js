1f6453c121135417757f39ead74c796df7b53c62
puhtimaki <113307512+puhtimaki@users.noreply.github.com>
2023-02-02 21:26:23 +0200

    expense adding

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 
 //statuc expenses
-let lainaStatis = (document.querySelector('#laina').innerHTML = 263)
+let lainaStatic = (document.querySelector('#laina').innerHTML = 263)
 let autoStatic = (document.querySelector('#auto').innerHTML = 500)
 let kauppaStatic = (document.querySelector('#kauppa').innerHTML = 350)
 let muutStatic = (document.querySelector('#muut').innerHTML = 40)
@@ -25,4 +25,30 @@ function addBudget() {
   let budgetTotal = document.querySelector('#amount')
   budgetTotal.innerHTML = budget
   document.querySelector('#total-amount').value = ''
+}
+
+//add expenses button
+let addExpensesButton = document.querySelector('#check-amount')
+addExpensesButton.addEventListener('click', addExpenses)
+//enter input
+let expensesInput = document.querySelector('#user-amount')
+expensesInput.addEventListener('keyup', (e) => {
+  e.preventDefault()
+  if (e.keyCode === 13) {
+    addExpensesButton.click()
+  }
+})
+
+//add expenses function
+function addExpenses() {
+  let expenses = document.querySelector('#user-amount').value
+  let expensesTotal = document.querySelector('#laina')
+  expensesTotal.innerHTML = expenses
+  document.querySelector('#user-amount').value = ''
+
+  // sum expenses to variables
+  let sum = parseInt(lainaStatic)
+
+  let sumTotal = document.querySelector('#laina')
+  sumTotal.innerHTML = sum
 }
