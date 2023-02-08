68d87d51e452f54bf26ab968e34a24702525d86a
puhtimaki <113307512+puhtimaki@users.noreply.github.com>
2023-02-08 17:57:17 +0200

    budget addings

--- script.js
+++ script.js
@@ -19,6 +19,19 @@ budgetInput.addEventListener('keyup', (e) => {
     AddbbudgetButton.click()
   }
 })
+
+//onclick event button
+let AddSavingsButton = document.querySelector('#total-saastotili-button')
+AddSavingsButton.addEventListener('click', addSavings)
+// eenter input
+let savingsInput = document.querySelector('#total-saastotili')
+savingsInput.addEventListener('keyup', (e) => {
+  e.preventDefault()
+  if (e.keyCode === 13) {
+    AddSavingsButton.click()
+  }
+})
+
 //add budget amount
 function addBudget() {
   let budget = document.querySelector('#total-amount').value
@@ -39,6 +52,13 @@ function addBudget() {
   decreaseBudgetTotal.innerHTML = decreaseBudget
 }
 
+function addSavings() {
+  let savings = document.querySelector('#total-saastotili').value
+  let savingsTotal = document.querySelector('#saastotili-amount')
+  savingsTotal.innerHTML = savings
+  document.querySelector('#total-saastotili').value = ''
+}
+
 //add expenses button
 let addExpensesButton = document.querySelector('#check-amount')
 addExpensesButton.addEventListener('click', addExpenses)
@@ -54,12 +74,47 @@ expensesInput.addEventListener('keyup', (e) => {
 
 //add expenses function
 function addExpenses() {
-  // sum expenses to lainastatic
+  //select from dropdown
+  let select = document.querySelector('#kategoriat')
+  let selectValue = select.options[select.selectedIndex].value
 
-  let laina = document.querySelector('#laina')
-  let lainaInput = document.querySelector('#user-amount').value
-  let lainaTotal = parseInt(lainaStatic) + parseInt(lainaInput)
-  laina.innerHTML = lainaTotal
-
-  document.querySelector('#user-amount').value = ''
+  if (selectValue === 'laina') {
+    let laina = document.querySelector('#laina')
+    let lainaInput = document.querySelector('#user-amount').value
+    let lainaTotal = parseInt(lainaStatic) + parseInt(lainaInput)
+    laina.innerHTML = lainaTotal
+    document.querySelector('#user-amount').value = ''
+  } else if (selectValue === 'auto') {
+    let auto = document.querySelector('#auto')
+    let autoInput = document.querySelector('#user-amount').value
+    let autoTotal = parseInt(autoStatic) + parseInt(autoInput)
+    auto.innerHTML = autoTotal
+    document.querySelector('#user-amount').value = ''
+  } else if (selectValue === 'kauppa') {
+    let kauppa = document.querySelector('#kauppa')
+    let kauppaInput = document.querySelector('#user-amount').value
+    let kauppaTotal = parseInt(kauppaStatic) + parseInt(kauppaInput)
+    kauppa.innerHTML = kauppaTotal
+    document.querySelector('#user-amount').value = ''
+  } else if (selectValue === 'muut') {
+    let muut = document.querySelector('#muut')
+    let muutInput = document.querySelector('#user-amount').value
+    let muutTotal = parseInt(muutStatic) + parseInt(muutInput)
+    muut.innerHTML = muutTotal
+    document.querySelector('#user-amount').value = ''
+  } else if (selectValue === 'viihde') {
+    let viihde = document.querySelector('#viihde')
+    let viihdeInput = document.querySelector('#user-amount').value
+    let viihdeTotal = parseInt(viihdeStatic) + parseInt(viihdeInput)
+    viihde.innerHTML = viihdeTotal
+    document.querySelector('#user-amount').value = ''
+  } else if (selectValue === 'netti') {
+    let netti = document.querySelector('#netti')
+    let nettiInput = document.querySelector('#user-amount').value
+    let nettiTotal = parseInt(nettiStatic) + parseInt(nettiInput)
+    netti.innerHTML = nettiTotal
+    document.querySelector('#user-amount').value = ''
+  } else {
+    alert('Täytä kentät!')
+  }
 }
